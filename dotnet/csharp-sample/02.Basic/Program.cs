bc38244ca9356d03bfa55910f107ef7e5740b512
techstay <lovery521@gmail.com>
2022-03-30 03:58:20 +0800

    ✨ .net

---
+++ dotnet/csharp-sample/02.Basic/Program.cs
@@ -0,0 +1,24 @@
+// 整数
+
+sbyte sb = 127;
+byte b = 255;
+short s = 30000;
+ushort us = 65535;
+int i;
+uint ui;
+long l;
+ulong ul;
+
+// 布尔型
+bool isChecked = true;
+
+// 小数
+float f;
+double d;
+
+// 高精度数字
+decimal dDecimal = 123475;
+
+// 字符类型
+char c = 'c';
+string text = "12345";
---
+++ dotnet/dotnet-libraries/hashids-sample/Program.cs
@@ -0,0 +1,30 @@
+﻿// 项目没有输出，使用调试功能来查看每个函数的结果
+using HashidsNet;
+
+var salt = "this is a salt.";
+var hashids = new Hashids(salt);
+
+// int id
+var hash = hashids.Encode(100);
+var id = hashids.Decode(hash);
+
+// 加密long id
+hash = hashids.EncodeLong(1000L);
+var id2 = hashids.DecodeLong(hash);
+
+// 加密多个ID，不会轻易的被猜出
+hash = hashids.Encode(1, 1, 1, 1, 1);
+id = hashids.Decode(hash);
+
+// 加密16进制字符串
+hash = hashids.EncodeHex("cafe");
+var hex = hashids.DecodeHex(hash);
+
+// 指定哈希最短长度
+hashids = new Hashids(salt, 8);
+hash = hashids.Encode(100);
+
+// 指定哈希字符串要使用的字符
+hashids = new Hashids(salt, 0, "ABCDEFGabcdefg1234567890");
+hash = hashids.Encode(100);
+id = hashids.Decode(hash);
