09630b2c4a38d2395eb8798ac2207eca633762be
techstay <lovery521@gmail.com>
2022-11-09 03:29:41 +0800

    ⬆️ 更新了项目版本

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -5,7 +5,7 @@ import { plugins } from './plugins'
 
 export default defineUserConfig({
   lang: 'zh-CN',
-  title: '碎碎念的学习笔记',
+  title: '落了一地碎碎念的学习笔记',
   description: '我的代码和笔记仓库',
   theme: defaultTheme({
     navbar: navbar,
--- .vuepress/plugins.ts
+++ .vuepress/plugins.ts
@@ -1,4 +1,12 @@
 import { PluginConfig } from '@vuepress/theme-default'
+import { searchPlugin } from '@vuepress/plugin-search'
+import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
 
 export const plugins: PluginConfig = [
+  searchPlugin({
+    isSearchable: (page) => page.path !== '/',
+  }),
+  googleAnalyticsPlugin({
+    id: 'G-29BS5TXBVL',
+  }),
 ]
