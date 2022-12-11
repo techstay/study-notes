86d3d96cd34cbfd85c1c4a41a9129958d3aada0f
techstay <lovery521@gmail.com>
2022-12-11 14:56:48 +0800

    Revert ":sparkles: 添加了教程部分"
    This reverts commit 1ac448001fef72d2d3f1bd91391068f91fbaf3a3.

--- .vuepress/navbar.ts
+++ .vuepress/navbar.ts
@@ -1,23 +1,12 @@
 import type { HopeThemeNavbarConfig } from "vuepress-theme-hope";
 
 export const navbar: HopeThemeNavbarConfig = [
-  {
-    text: '笔记',
-    prefix: '/notes/',
-    icon: 'note',
-    children: [
-      'windows/',
-      'linux/',
-      'network/',
-      'android/',
-      'misc/',
-      'software/',
-      'creativity/',
-      'troubleshooting/',
-    ]
-  },
-  {
-    text: '实用技术',
-    link: '/tutorial/skill/'
-  }
+  '/notes/windows/',
+  '/notes/linux/',
+  '/notes/network/',
+  '/notes/android/',
+  '/notes/misc/',
+  '/notes/software/',
+  '/notes/creativity/',
+  '/notes/troubleshooting/',
 ]
--- .vuepress/sidebar.ts
+++ .vuepress/sidebar.ts
@@ -4,11 +4,10 @@ export const sidebar: HopeThemeSidebarConfig =
 {
   "/notes/windows/": "structure",
   "/notes/linux/": "structure",
+  "/notes/android/": "structure",
   "/notes/network/": "structure",
   "/notes/misc/": "structure",
   "/notes/software/": "structure",
   "/notes/creativity/": "structure",
   "/notes/troubleshooting/": "structure",
-  "/notes/android/": "structure",
-  "/tutorial/skill/": "structure",
 }
