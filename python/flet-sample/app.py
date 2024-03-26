c09df8b4d76b93b2475d754fe3b20467f0b2a383
techstay <lovery521@gmail.com>
2024-03-27 02:27:00 +0800

    ⬆️ update flet

--- python/flet-sample/app.py
+++ python/flet-sample/app.py
@@ -1,20 +1,34 @@
-import flet
+import logging
 
+import flet
 from flet import IconButton, Page, Row, TextField, icons
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("flet")
+logger.setLevel(logging.INFO)
 
 
 def main(page: Page):
     page.title = "Flet counter example"
-    page.vertical_alignment = "center"
+    page.vertical_alignment = flet.MainAxisAlignment.CENTER
+    page.window_center()
+    page.window_width = 300
+    page.window_height = 200
 
-    txt_number = TextField(value="0", text_align="right", width=100)
+    txt_number = TextField(
+        value="0",
+        text_align=flet.TextAlign.RIGHT,
+        width=100,
+    )
 
     def minus_click(e):
-        txt_number.value = int(txt_number.value) - 1
+        txt_number.value = str(int(txt_number.value) - 1)
+        logger.info(f"minus_click: {txt_number.value}")
         page.update()
 
     def plus_click(e):
-        txt_number.value = int(txt_number.value) + 1
+        txt_number.value = str(int(txt_number.value) + 1)
+        logger.info(f"plus_click: {txt_number.value}")
         page.update()
 
     page.add(
@@ -24,9 +38,10 @@ def main(page: Page):
                 txt_number,
                 IconButton(icons.ADD, on_click=plus_click),
             ],
-            alignment="center",
+            alignment=flet.MainAxisAlignment.CENTER,
         )
     )
 
 
-flet.app(target=main)
+if __name__ == "__main__":
+    flet.app(target=main)
