18069488c2c1b2576a4a96e5c052dec99c40f134
techstay <techstay@proton.me>
2025-09-21 02:05:20 +0800

    java: add java 25 example to java-samples

--- java/java-samples/build.gradle.kts
+++ java/java-samples/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
+        languageVersion.set(JavaLanguageVersion.of(25))
     }
 }
 
---
+++ java/java-samples/src/main/java/tech/techstay/function/CompactSourceFiles.java
@@ -0,0 +1,4 @@
+// Top level functions, available since Java 25
+void main() {
+  System.out.println("Hello World");
+}
